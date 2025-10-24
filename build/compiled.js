bb0e18822ff63dfaaee11587e1cc5dd54ae250ea
Matheus Felipe <matheus@fedora.linuxmint>
2025-10-24 09:23:44 -0300

    Add: function that opens a weekly note based on a title constant

--- build/compiled.js
+++ build/compiled.js
@@ -1,37 +1,21 @@
 (() => {
-  // lib/arbitrary-plugin-module.js
-  async function proofImportIsPossible() {
-    console.log("As you can see, code is being run that originated from a different file than the base plugin.js file \u2728");
-    return true;
-  }
-
   // lib/plugin.js
   var plugin = {
-    // --------------------------------------------------------------------------------------
-    constants: {},
-    // --------------------------------------------------------------------------
-    // https://www.amplenote.com/help/developing_amplenote_plugins#insertText
-    insertText: {},
-    // --------------------------------------------------------------------------
-    // https://www.amplenote.com/help/developing_amplenote_plugins#noteOption
-    noteOption: {
-      "Baby's first Note Option command": {
-        check: async function(app, noteUUID) {
-          const noteContent = await app.getNoteContent({ uuid: noteUUID });
-          const proof = proofImportIsPossible();
-          return /cool/i.test(noteContent.toLowerCase());
-        },
-        run: async function(app, noteUUID) {
-          await app.alert("You clicked the Baby's first Note Option command in a COOL note!");
-          console.debug("Special message to the DevTools console");
+    constants: {
+      weeklyNoteTitle: "Weekly Note W42"
+    },
+    appOption: {
+      "Open weekly note": async function(app) {
+        const weeklyNote = await app.findNote({ title: plugin.constants.weeklyNoteTitle });
+        if (weeklyNote) {
+          await app.navigate(`https://amplenote.com/notes/${weeklyNote.uuid}`);
+        } else {
+          await app.alert("No weekly note found");
+          console.error("No weekly note found");
+          console.error("Weekly note title: ", plugin.constants.weeklyNoteTitle);
         }
       }
-    },
-    // --------------------------------------------------------------------------
-    // https://www.amplenote.com/help/developing_amplenote_plugins#replaceText
-    replaceText: {}
-    // There are several other entry points available, check them out here: https://www.amplenote.com/help/developing_amplenote_plugins#Actions
-    // You can delete any of the insertText/noteOptions/replaceText keys if you don't need them
+    }
   };
   var plugin_default = plugin;
 })();
