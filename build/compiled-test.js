2972a0400855791ee1677f3078d6c486daf5f101
Matheus Felipe <theusf40@hotmail.com>
2025-10-24 09:56:03 -0300

    Add script for building a test artifact

---
+++ build/compiled-test.js
@@ -0,0 +1,19 @@
+(() => {
+  // lib/plugin.js
+  var plugin = {
+    constants: {
+      weeklyNoteTitle: "Weekly Note W42"
+    },
+    appOption: {
+      "Open weekly note": async function(app) {
+        let weeklyNote = await app.findNote({ title: plugin.constants.weeklyNoteTitle });
+        if (!weeklyNote) {
+          console.log("No weekly note found, creating new one");
+          weeklyNote = await createNote(app, plugin.constants.weeklyNoteTitle);
+        }
+        await app.navigate(`https://amplenote.com/notes/${weeklyNote.uuid}`);
+      }
+    }
+  };
+  var plugin_default = plugin;
+})();
